068ba5a1b31e0baca522438eb36f99543bd66a60
Aavash Dhami <dhamiakkal21@gmail.com>
2024-11-18 19:29:41 +0545

    Add files via upload

---
+++ Day-2/index.js
@@ -0,0 +1,30 @@
+// var x = 1;
+// a();
+// b();
+
+// console.log(x);
+
+// function a() {
+//   var x = 10;
+//   console.log(x);
+// }
+
+// function b() {
+//   var x = 100;
+//   console.log(x);
+// }
+
+//! Undefined : a special placeholder , used  to reserve memory for the variables in the memory creation phase.
+
+console.log(y); // undefined
+var y = 10;
+
+console.log(z); // undefined
+var z = 10;
+
+greet();
+function greet() {
+  console.log("Hello World!");
+}
+
+// console.log(xasdf); // ReferenceError: x is not defined
